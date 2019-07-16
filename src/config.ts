eae75d8b2b00661a665ecb1938fd7a898cc3253f
Gabriel V <gabriel.vial@irot.cl>
2019-07-16 17:16:16 -0400

    [UPD] Updated public ip for backend in aws

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const Configuration = {
   api: {
-    base: 'http://localhost:3000',
+    // base: 'http://localhost:3000',
+    base: '3.15.158.237:3000',
   },
   Refresh: {
     Timeout: 10 * 1000
