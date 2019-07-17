951a068c9cef60953b4969a1886441ef3552753c
Gabriel V <gabriel.vial@irot.cl>
2019-07-17 14:09:33 -0400

    [UPD] Added new ip aws EC2

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const Configuration = {
   api: {
-    base: 'http://localhost:3000',
-    // base: 'http://3.130.190.128:3000',
+    // base: 'http://localhost:3000',
+    base: 'http://18.220.151.197:3000',
   },
   Refresh: {
     Timeout: 10 * 1000
