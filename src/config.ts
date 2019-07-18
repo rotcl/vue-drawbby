936b08ea8ec0d536a3bc67a74eb564fff1fd4ff4
Gabriel V <gabriel.vial@irot.cl>
2019-07-18 01:09:03 -0400

    [UPD] Added Nginx to aws server, so I modified the original api route

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const Configuration = {
   api: {
     // base: 'http://localhost:3000',
-    base: 'http://18.220.151.197:3000',
+    base: 'http://18.220.151.197',
   },
   Refresh: {
     Timeout: 10 * 1000
