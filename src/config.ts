a3ed01aee8122d49f2928917ace997d694d6e700
Gabriel V <gabriel.vial@irot.cl>
2019-07-31 18:28:19 -0400

    [UPD] Mostrar local

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 export const Configuration = {
   api: {
-    // base: 'http://localhost:3000',
-    base: 'https://drawserver.ml',
+    base: 'http://localhost:3000',
+    // base: 'https://drawserver.ml',
+    // base: '18.220.151.197'
   },
   Refresh: {
     Timeout: 10 * 1000
